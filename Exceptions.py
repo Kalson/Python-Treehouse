37b6a474cfb2515de5f4e5f0316220c0298584b4
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-09-28 16:46:40 -0700

    Update

--- Exceptions.py
+++ Exceptions.py
@@ -1,6 +1,6 @@
 # get two inputs
-user_string = input("What's your word?")
-user_num = input("What's your number?")
+user_string = input("What's your word? ")
+user_num = input("What's your number? ")
 
 # convert the strings into numbers
 try:
@@ -15,3 +15,12 @@ if "." not in user_num:
 else:
     ratio = round(len(user_string)*our_num)
     print(user_string[ratio])
+
+# try - A keyword that marks a block of code to be tried that will watch for exceptions to happen.
+# len - gets the length of something
+# except - A keyword that catches exceptions and runs some other code when one happens.
+# Exception - A catchable/predictable mistake in code, like trying to turn the string 'a' into a number, which would throw a ValueError exception.
+# not - A keyword that negates whatever follows it.
+# in - A keyword that checks for membership.
+# round() - A function that rounds a float to the nearest integer(whole number).
+# Index - The position of something inside of something else. This will be a number and counting starts at zero.
