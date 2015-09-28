ba30ba30d629513aef0e6dc0cf3d44a534de8fed
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-09-28 15:36:22 -0700

    Update

--- string_multiply.py
+++ string_multiply.py
@@ -2,3 +2,5 @@ string_input = input("What string do you want to use?")
 string_int = input("How many times do you want to repeat the string?")
 
 print(string_input*int(string_int))
+
+# practicing use of mutiply strings with integers
