34e171c706489ed13547e9e7bf84628497580ded
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-09-28 13:58:32 -0700

    String Concatenation

---
+++ lumberjack.py
@@ -0,0 +1,6 @@
+name = input("What's your name?")
+
+if name == "Kalson":
+    print(name + " is a lumberjack")
+else:
+    print("Well first of, he's super lazy and " + name + " is not a lumberjack")
