03c772297a2bef229f8d129c3c399d54623c82f0
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-09-29 13:06:54 -0700

    added triple quotes for comments

--- Exceptions.py
+++ Exceptions.py
@@ -16,11 +16,13 @@ else:
     ratio = round(len(user_string)*our_num)
     print(user_string[ratio])
 
-# try - A keyword that marks a block of code to be tried that will watch for exceptions to happen.
-# len - gets the length of something
-# except - A keyword that catches exceptions and runs some other code when one happens.
-# Exception - A catchable/predictable mistake in code, like trying to turn the string 'a' into a number, which would throw a ValueError exception.
-# not - A keyword that negates whatever follows it.
-# in - A keyword that checks for membership.
-# round() - A function that rounds a float to the nearest integer(whole number).
-# Index - The position of something inside of something else. This will be a number and counting starts at zero.
+"""
+try - A keyword that marks a block of code to be tried that will watch for exceptions to happen.
+len - gets the length of something like a string
+except - A keyword that catches exceptions and runs some other code when one happens.
+Exception - A catchable/predictable mistake in code, like trying to turn the string 'a' into a number, which would throw a ValueError exception.
+not - A keyword that negates whatever follows it.
+in - A keyword that checks if something is in something else
+round() - A function that rounds a float to the nearest integer(whole number).
+Index - The position of something inside of something else. This will be a number and counting starts at zero.
+"""
