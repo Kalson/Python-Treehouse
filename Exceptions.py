8b5b7cc4135b31706f2014747bd1188f55416e2d
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-09-30 15:55:21 -0700

    Updated

--- Exceptions.py
+++ Exceptions.py
@@ -17,12 +17,16 @@ else:
     print(user_string[ratio])
 
 """
-try - A keyword that marks a block of code to be tried that will watch for exceptions to happen.
+try - A keyword that marks a block of code to be tried that will watch for
+exceptions to happen.
 len - gets the length of something like a string
-except - A keyword that catches exceptions and runs some other code when one happens.
-Exception - A catchable/predictable mistake in code, like trying to turn the string 'a' into a number, which would throw a ValueError exception.
+except - A keyword that catches exceptions and runs some other code when one
+happens.
+Exception - A catchable/predictable mistake in code, like trying to turn the
+string 'a' into a number, which would throw a ValueError exception.
 not - A keyword that negates whatever follows it.
 in - A keyword that checks if something is in something else
 round() - A function that rounds a float to the nearest integer(whole number).
-Index - The position of something inside of something else. This will be a number and counting starts at zero.
+Index - The position of something inside of something else. This will be a
+number and counting starts at zero.
 """
