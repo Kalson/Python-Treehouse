301366899ecd574443f9dc94fd643383490b4b31
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-10-05 16:49:30 -0700

    no changes

--- Functions.py
+++ Functions.py
@@ -3,10 +3,12 @@ def say_hello():
 
 say_hello()
 
+
 def add_to_two(int):
     print(int + 2)
 
 add_to_two(4)
+
 
 def square(num):
     return num * num
@@ -15,3 +17,14 @@ print(square(5))
 
 two_squared = square(2)
 print(two_squared)
+
+
+def new_function(num):
+    # kool = num * 3
+    return num * 10
+
+answer = new_function(3)
+print(answer)
+
+new_answer = new_function(4)
+print(new_answer)
