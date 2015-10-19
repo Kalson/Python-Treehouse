66fffc78de7a3db14e3e8ae3293807fed1265566
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-10-19 01:57:26 -0700

    another __init__ methods - monster.py

--- monster.py
+++ monster.py
@@ -1,15 +1,15 @@
 class Monster:
-    def __init__(self, hit_points, weapon, color, sound):
-        self.hit_points = hit_points
-        self.color = color
-        self.weapon = weapon
-        self.sound = sound
+    def __init__(self, **sooo):
+        self.hit_points = sooo.get("hit_points", 1)
+        self.color = sooo.get("color", "green")
+        self.weapon = sooo.get("weapon", "sword")
+        self.sound = sooo.get("sound", "woof")
 
     def battlecry(self):
         return self.sound.upper()
 
 
-bigfoot = Monster("1", "sword", "green", "meow")
+bigfoot = Monster(color="red", sound="tweet")
 print(bigfoot.color)
-print(bigfoot.weapon.upper())
+print(bigfoot.hit_points)
 print(bigfoot.battlecry())
