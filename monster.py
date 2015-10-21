6c7763b0cc5e419565e3d351e78dcd2bba6e4177
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-10-21 15:12:42 -0700

    Intro to Inheritance - monster.py

--- monster.py
+++ monster.py
@@ -1,15 +1,32 @@
+import random
+COLORS = ["red", "green", "blue", "white"]
+
+
 class Monster:
-    def __init__(self, **sooo):
-        self.hit_points = sooo.get("hit_points", 1)
-        self.color = sooo.get("color", "green")
-        self.weapon = sooo.get("weapon", "sword")
-        self.sound = sooo.get("sound", "woof")
+    max_hit_points = 1
+    min_hit_points = 1
+    max_exp = 1
+    min_exp = 1
+    weapon = "sword"
+    sound = "yeti-roar"
+
+    def __init__(self, **kwargs):
+        self.hit_points = random.randint(self.max_hit_points, self.min_hit_points)
+        self.exp = random.randint(self.max_exp, self.min_exp)
+        self.color = random.choice(COLORS)
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+            # setattr takes 3 arg:
+            # self - the object to set the attribute on
+            # key - the attribute we want to set
+            # value - to give the attribute
 
     def battlecry(self):
         return self.sound.upper()
 
 
-bigfoot = Monster(color="red", sound="tweet")
+bigfoot = Monster()
 print(bigfoot.color)
 print(bigfoot.hit_points)
 print(bigfoot.battlecry())
