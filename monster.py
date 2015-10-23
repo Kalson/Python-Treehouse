dcba77e64a3810db59f054258bc57884db4f38c3
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-10-22 21:22:00 -0700

    Subclasses - monster.py

--- monster.py
+++ monster.py
@@ -11,8 +11,8 @@ class Monster:
     sound = "yeti-roar"
 
     def __init__(self, **kwargs):
-        self.hit_points = random.randint(self.max_hit_points, self.min_hit_points)
-        self.exp = random.randint(self.max_exp, self.min_exp)
+        self.hit_points = random.randint(self.min_hit_points, self.max_hit_points)
+        self.exp = random.randint(self.min_exp, self.max_exp)
         self.color = random.choice(COLORS)
 
         for key, value in kwargs.items():
@@ -26,15 +26,43 @@ class Monster:
         return self.sound.upper()
 
 
-bigfoot = Monster()
-print(bigfoot.color)
-print(bigfoot.hit_points)
-print(bigfoot.battlecry())
+class Goblin(Monster):
+    max_hit_points = 3
+    min_hit_points = 3
+    max_exp = 2
+    min_exp = 2
+    sound = "sqeak"
 
-print("=========")
 
-lochness = Monster(color="blue-green", hit_points=500, sound="yawn", adjective="manxsome")
-print(lochness.color)
-print(lochness.hit_points)
-print(lochness.adjective)
-print(lochness.battlecry())
+class Troll(Monster):
+    min_hit_points = 3
+    max_hit_points = 5
+    min_exp = 2
+    max_exp = 6
+    sound = "growl"
+
+
+class Dragon(Monster):
+    min_hit_points = 5
+    max_hit_points = 10
+    min_exp = 6
+    max_exp = 10
+    sound = "raaaaaaaaaar"
+
+Matt = Goblin()
+print(Matt.hit_points)
+print(Matt.exp)
+print(Matt.color)
+print(Matt.battlecry())
+
+Dave = Troll()
+print(Dave.hit_points)
+print(Dave.exp)
+print(Dave.color)
+print(Dave.battlecry())
+
+Drake = Dragon()
+print(Drake.hit_points)
+print(Drake.exp)
+print(Drake.color)
+print(Drake.battlecry())
